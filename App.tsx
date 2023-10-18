a092ffaeec56b5c186ca157798e4e548469bb044
dpedersoli <dpmsengineer@gmail.com>
2023-10-18 15:33:57 -0300

    feat: SignIn screen & Loading and Input components & png and svg typage

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
+import { Loading } from "@components/loading";
 import {
   Roboto_400Regular,
   Roboto_700Bold,
   useFonts,
 } from "@expo-google-fonts/roboto";
-import { StatusBar, Text, View } from "react-native";
+import { SignIn } from "@screens/SignIn";
+import { NativeBaseProvider } from "native-base";
+import { StatusBar } from "react-native";
+import { THEME } from "./src/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -11,20 +15,13 @@ export default function App() {
     Roboto_700Bold,
   });
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#202024",
-      }}
-    >
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Text>Hello World!</Text> : <View></View>}
-    </View>
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
   );
 }
