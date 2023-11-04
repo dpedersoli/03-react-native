f4cddebf722bbaaaab3cc87f2f7fd9eb53f831ce
dpedersoli <dpmsengineer@gmail.com>
2023-11-03 21:35:13 -0300

    feat: skeleton native-base

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,36 @@
-import { Center, Text } from "native-base";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import { Center, ScrollView, Skeleton, VStack } from "native-base";
+import { useState } from "react";
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
+
   return (
-    <Center flex={1}>
-      <Text color="white">Profilee</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView>
+        <Center mt={6} px={10}>
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: "http://github.com/dpedersoli.png" }}
+              alt="Foto do usuário"
+              size={PHOTO_SIZE}
+            />
+          )}
+        </Center>
+      </ScrollView>
+    </VStack>
   );
 }
