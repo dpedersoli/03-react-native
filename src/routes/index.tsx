2589c2d976a41692ac388a9f5b153c4a75a72b33
dpedersoli <dpmsengineer@gmail.com>
2023-12-04 15:31:41 -0300

    feat: redirect user according with his authContext

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,12 @@
 import { useAuth } from "@hooks/userAuth";
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { Box, useTheme } from "native-base";
+import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
   const { colors } = useTheme();
   const { user } = useAuth();
-
-  console.log("USUÁRIO LOGADO => ", user);
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
@@ -15,7 +14,8 @@ export function Routes() {
   return (
     <Box flex={1} bg="gray.700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
+        {/* Se o usuário estiver logado, então ele pode navegar nas rotas da aplicação "AppRoutes", caso contrário, irá rodar nas rotas de/para autenticação "AuthRoutes"*/}
       </NavigationContainer>
     </Box>
   );
